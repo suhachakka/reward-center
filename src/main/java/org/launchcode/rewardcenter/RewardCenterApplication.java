d6dc8d4246db0815e5dde7a66611e42d703dacc6
suha <suhasinichakka@gmail.com>
2019-01-27 01:13:49 -0600

    Initial commit

---
+++ src/main/java/org/launchcode/rewardcenter/RewardCenterApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.rewardcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardCenterApplication.class, args);
+	}
+
+}
+
